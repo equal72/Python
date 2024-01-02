57ab296e707af63d2e4a1e09e96a7367d31378dd
equal72 <equal72.bae@gmail.com>
2024-01-02 17:41:52 +0900

    compare two voxels one by one

---
+++ voxel/compare.py
@@ -0,0 +1,25 @@
+
+def compare_voxels(file1: str, file2: str) -> int:
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        out1, x1, y1, z1 = f1.readline().split()
+        out2, x2, y2, z2 = f2.readline().split()
+        NumVoxel = 1
+        for line1, line2 in zip(f1, f2):
+            step1, mat1, den1 = line1.split()
+            step2, mat2, den2 = line2.split()
+
+            if (step1 != step2 and mat1 != mat2 and float(den1) != float(den2)):
+                return False
+            NumVoxel += 1
+        print(f"{x1}*{y1}*{z1}={NumVoxel-1} voxels are compared")
+        return True
+
+with open('input.txt', 'r') as input:
+    for line in input:
+        item, ref, new = line.strip().split()
+        if compare_voxels(ref, new):
+            print(f"{item}: The files are identical")
+        else:
+            print(f"{item}: The files are different")
+
+      
